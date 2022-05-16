48f89e7c2d3f0684ae5c42492ab048d9a43b2926
GRND0 <zefiro.turchetta@gmail.com>
2022-05-16 15:17:53 +0200

    prima milestone

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: "AppTitle",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
